8d31d8f205badd71b27229adabcf8c1e89900596
Sotto <ms@42lisboa.com>
2022-04-08 09:45:32 +0100

    Added options to init.lua

---
+++ init.lua
@@ -0,0 +1,11 @@
+-------------------------------------------------------------------------------
+-- init.lua
+-- User: mlanca-c
+-- URL: http://github.com/mlanca-c/nvim
+-- Version: 0.1
+--
+-- Description: This is the main init configuration file for neovim.
+-------------------------------------------------------------------------------
+
+-- This calls the 'lua/user/options.lua' file
+require "user.options"
