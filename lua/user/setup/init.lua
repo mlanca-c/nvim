2f3b010830b7514607c8eb003713a8a910610be3
Sotto <ms@42lisboa.com>
2022-04-21 17:06:30 +0100

    Updated init.lua Added treesitter, lsp, and telescope

--- lua/user/setup/init.lua
+++ lua/user/setup/init.lua
@@ -7,7 +7,9 @@
 
 -- This calls the 'lua/user/cmp.lua' file
 require "user.setup.cmp"
--- -- This calls the 'lua/user/telescope.lua' file
--- require "user.setup.telescope"
--- -- This calls the 'lua/user/treesitter.lua' file
--- require "user.setup.treesitter"
+-- This calls the 'lua/user/treesitter.lua' file
+require "user.setup.treesitter"
+-- This calls the 'lua/user/lsp' directory
+require "user.setup.lsp"
+-- This calls the 'lua/user/telescope.lua' file
+require "user.setup.telescope"
