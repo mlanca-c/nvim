874fe6bd7a32d7173728982d0ab958be6c97ee9e
Sotto <ms@42lisboa.com>
2022-04-21 17:06:58 +0100

    Removed null-ls

--- lua/user/setup/lsp/init.lua
+++ lua/user/setup/lsp/init.lua
@@ -16,5 +16,3 @@ end
 require "user.setup.lsp.lsp-installer"
 -- This calls the 'lua/user/setup/lsp/handlers.lua' setup() function
 require ("user.setup.lsp.handlers").setup()
--- This calls the 'lua/user/setup/lsp/null-ls.lua' file
-require "user.setup.lsp.null-ls"
