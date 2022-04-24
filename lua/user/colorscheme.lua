c3cc58ce600fcccd800070da9f63b611069bea25
mlanca-c <marialsottomayor@gmail.com>
2022-04-24 16:52:10 +0100

    Small config change

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -5,23 +5,14 @@
 -- Version: 2.0
 -------------------------------------------------------------------------------
 
--- Colorscheme:
-local colorscheme = "sonokai"
+-- Colorscheme: sonokai
+-- source: https://github.com/LunarVim/Neovim-from-scratch
 
--- protected call - instead of vim.cmd "colorscheme <colorscheme>"
-local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
-
--- Confirming operation
-if not status_ok then
-	vim.notify("colorscheme \'" .. colorscheme .. "\' not found!")
-	return
-end
-
--- vim.cmd [[
--- try
---   colorscheme sonokai 
--- catch /^Vim\%((\a\+)\)\=:E185/
---   colorscheme default
---   set background=dark
--- endtry
--- ]]
+vim.cmd [[
+try
+  colorscheme sonokai
+catch /^Vim\%((\a\+)\)\=:E185/
+  colorscheme default
+  set background=dark
+endtry
+]]
